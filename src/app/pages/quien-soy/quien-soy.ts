45199908ccf5cf5608ba28eb64d19814457ca9f4
Leonel Gadea <leomaxgadea@gmail.com>
2026-05-18 19:49:51 -0300

    Agrego quien soy con API de GitHub

--- src/app/pages/quien-soy/quien-soy.ts
+++ src/app/pages/quien-soy/quien-soy.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quien-soy',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quien-soy.html',
-  styleUrl: './quien-soy.css',
+  styleUrl: './quien-soy.css'
 })
-export class QuienSoy {}
+export class QuienSoy implements OnInit {
+
+  usuario: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+
+    this.http.get('https://api.github.com/users/Leoooo19')
+      .subscribe(data => {
+
+        this.usuario = data;
+        console.log(data);
+
+      });
+
+  }
+
+}
