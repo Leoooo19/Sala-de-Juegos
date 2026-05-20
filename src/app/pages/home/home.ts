5b7eb5614627b5b778f143dfe38f64080b0686ca
Leonel Gadea <leomaxgadea@gmail.com>
2026-05-19 21:18:13 -0300

    Sprint 2 prueba

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements OnInit {
+
+  usuario: any = null;
+
+  constructor(private authService: AuthService) {}
+
+  async ngOnInit() {
+    const { data } = await this.authService.obtenerUsuario();
+    this.usuario = data.user;
+  }
+
+  async cerrarSesion() {
+    await this.authService.logout();
+    this.usuario = null;
+  }
+}
