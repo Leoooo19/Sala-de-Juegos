cfded8b0f5f0af501143433be71068c3eb37538c
Leonel Gadea <leomaxgadea@gmail.com>
2026-05-26 01:24:19 -0300

    Prueba Sprint

--- src/app/pages/preguntados/preguntados.ts
+++ src/app/pages/preguntados/preguntados.ts
@@ -9,58 +9,58 @@ import { CommonModule } from '@angular/common';
 })
 export class Preguntados {
 
-  preguntas = [
-    {
-      pregunta: '¿Que club argentino es conocido como El Millonario?',
-      opciones: ['Boca Juniors', 'River Plate', 'Racing Club', 'Independiente'],
-      correcta: 'River Plate'
-    },
-    {
-      pregunta: '¿Que clásico juegan Boca Juniors y River Plate?',
-      opciones: ['Clasico de Avellaneda', 'Clasico Rosarino', 'Superclásico', 'Clasico Platense'],
-      correcta: 'Superclásico'
-    },
-    {
-      pregunta: '¿Que club juega de local en el estadio Presidente Perón?',
-      opciones: ['Racing Club', 'Independiente', 'San Lorenzo', 'Huracan'],
-      correcta: 'Racing Club'
-    },
-    {
-      pregunta: '¿Que equipo es conocido como El Rojo?',
-      opciones: ['Argentinos Juniors', 'Independiente', 'Estudiantes', 'Newell’s'],
-      correcta: 'Independiente'
-    },
-    {
-      pregunta: '¿Que clásico juegan Newell’s y Rosario Central?',
-      opciones: ['Clásico Rosarino', 'Clásico Santafesino', 'Clásico Cordobés', 'Clásico del Oeste'],
-      correcta: 'Clásico Rosarino'
-    },
-    {
-      pregunta: '¿Que club argentino tiene como apodo El Ciclon?',
-      opciones: ['Huracán', 'San Lorenzo', 'Vélez', 'Lanus'],
-      correcta: 'San Lorenzo'
-    },
-    {
-      pregunta: '¿Que club es conocido como El Pincha?',
-      opciones: ['Gimnasia LP', 'Estudiantes LP', 'Colón', 'Unión'],
-      correcta: 'Estudiantes LP'
-    },
-    {
-      pregunta: '¿Que clasico juegan Racing e Independiente?',
-      opciones: ['Clasico Porteño', 'Clasico de Avellaneda', 'Clasico del Sur', 'Clasico Nacional'],
-      correcta: 'Clasico de Avellaneda'
-    },
-    {
-      pregunta: '¿Que club argentino es conocido como El Fortin?',
-      opciones: ['Velez Sarsfield', 'Banfield', 'Tigre', 'Arsenal'],
-      correcta: 'Velez Sarsfield'
-    },
-    {
-      pregunta: '¿Que equipo es conocido como El Globo?',
-      opciones: ['San Lorenzo', 'Huracán', 'Platense', 'Chacarita'],
-      correcta: 'Huracan'
-    }
-  ];
+preguntas = [
+  {
+    pregunta: 'Que club argentino es conocido como El Millonario?',
+    opciones: ['Boca Juniors', 'River Plate', 'Racing Club', 'Independiente'],
+    correcta: 'River Plate'
+  },
+  {
+    pregunta: 'Que clasico juegan Boca Juniors y River Plate?',
+    opciones: ['Clasico de Avellaneda', 'Clasico Rosarino', 'Superclasico', 'Clasico Platense'],
+    correcta: 'Superclasico'
+  },
+  {
+    pregunta: 'Que club juega de local en el estadio Presidente Peron?',
+    opciones: ['Racing Club', 'Independiente', 'San Lorenzo', 'Huracan'],
+    correcta: 'Racing Club'
+  },
+  {
+    pregunta: 'Que equipo es conocido como El Rojo?',
+    opciones: ['Argentinos Juniors', 'Independiente', 'Estudiantes', 'Newells'],
+    correcta: 'Independiente'
+  },
+  {
+    pregunta: 'Que clasico juegan Newells y Rosario Central?',
+    opciones: ['Clasico Rosarino', 'Clasico Santafesino', 'Clasico Cordobes', 'Clasico del Oeste'],
+    correcta: 'Clasico Rosarino'
+  },
+  {
+    pregunta: 'Que club argentino tiene como apodo El Ciclon?',
+    opciones: ['Huracan', 'San Lorenzo', 'Velez', 'Lanus'],
+    correcta: 'San Lorenzo'
+  },
+  {
+    pregunta: 'Que club es conocido como El Pincha?',
+    opciones: ['Gimnasia LP', 'Estudiantes LP', 'Colon', 'Union'],
+    correcta: 'Estudiantes LP'
+  },
+  {
+    pregunta: 'Que clasico juegan Racing e Independiente?',
+    opciones: ['Clasico Porteno', 'Clasico de Avellaneda', 'Clasico del Sur', 'Clasico Nacional'],
+    correcta: 'Clasico de Avellaneda'
+  },
+  {
+    pregunta: 'Que club argentino es conocido como El Fortin?',
+    opciones: ['Velez Sarsfield', 'Banfield', 'Tigre', 'Arsenal'],
+    correcta: 'Velez Sarsfield'
+  },
+  {
+    pregunta: 'Que equipo es conocido como El Globo?',
+    opciones: ['San Lorenzo', 'Huracan', 'Platense', 'Chacarita'],
+    correcta: 'Huracan'
+  }
+];
 
   indicePregunta = 0;
   puntaje = 0;
